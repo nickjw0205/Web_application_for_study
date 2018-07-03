39a45547ed4c47010d079544f5c4c02b40237761
namadjw12 <jwsuh0703@gmail.com>
2018-07-03 19:08:35 +0900

    type_of_data_and_operator

---
+++ apache2/htdocs/phpjs/2.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+</head>
+<body>
+	<h1>
+		javascript
+	</h1>
+
+	<script>
+		document.write("10" + "10"); //-> 1010
+	</script>
+
+	<h2>
+		php
+	</h2>
+
+	<?php
+		echo "10" + "10"; //-> 20
+		//php에서의 +은 숫자를 더할때 쓰는거라고 약속 되있음
+		// 문자열을 더하려면 .을 씀
+		echo "10"."10"; //-> 1010
+	?>
+</body>
+</html>
