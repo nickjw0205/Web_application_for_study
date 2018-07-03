1367f20915dea2c80531b96b2fce1411f766835b
namadjw12 <jwsuh0703@gmail.com>
2018-07-03 19:02:46 +0900

    insert_code_into_webpage

---
+++ apache2/htdocs/1.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+</head>
+<body>
+	<h1>
+		php
+	</h1>
+
+	<?php 
+		// 문자를 출력할때는 echo!
+		// echo 10 + 10;   -> 20이 출력됨
+		echo 10+10;
+ 	?>
+
+ 	<h1>
+		Javascript
+	</h1>
+
+	<script>
+		document.write(10+10);
+	</script>
+
+</body>
+</html>
